c55d068bb7034a0bfa8396bfad305266d3908af7
Mark Kabugi <kabug222@gmail.com>
2025-07-31 16:42:13 +0300

    ht

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main style={{ padding: '15px', backgroundColor: '#f5f5f5', fontSize: '16px' }}>
+    <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
