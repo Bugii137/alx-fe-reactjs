87705deae9e1dd1095d9aba28f9c7eb5303a81d9
Mark Kabugi <kabug222@gmail.com>
2025-08-07 19:51:27 +0300

    Update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,10 +7,10 @@ function UserProfile() {
         className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300">
-        John Doe
+        Mark Kabugi
       </h1>
       <p className="text-sm md:text-base text-gray-600 text-center">
-        Developer at Example Co. Loves to write code and explore new technologies.
+        Software Developer | Tech Enthusiast 
       </p>
     </div>
   );
