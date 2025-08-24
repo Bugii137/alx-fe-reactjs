23ebefe603f414f2f6c90e2f2ac508aa1e0218d4
Mark Kabugi <kabug222@gmail.com>
2025-08-24 11:09:47 +0300

    react router setup main component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,36 +1,30 @@
-// src/App.jsx
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
-import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <BrowserRouter>
       <Routes>
+        {/* Public route */}
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
 
-        {/* Protected Profile with nested routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/profile" element={<Profile />}>
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-        </Route>
+        {/* Protected Profile route with nested routes */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
-        {/* Dynamic Route for blog */}
-        <Route path="/blog" element={<Blog />} />
+        {/* Dynamic blog post route */}
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
