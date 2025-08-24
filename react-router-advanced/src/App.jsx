da8a62385bcd69dd5f79ba4a2d14bd1aa1e91395
Mark Kabugi <kabug222@gmail.com>
2025-08-24 11:33:21 +0300

    routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,36 @@
-import FormikForm from "./components/FormikForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogPost from "./pages/BlogPost";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-      <FormikForm />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Dynamic blog route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
+
+        {/* Protected + nested routes */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
